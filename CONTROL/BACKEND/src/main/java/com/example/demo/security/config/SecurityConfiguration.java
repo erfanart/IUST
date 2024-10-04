0f6d9456865aa5c49d489231a90d9fe21d40cedd
MMDN82 <mahdidabiri1382@gmail.com>
2024-10-04 12:07:09 +0000

    FIRST

--- CONTROL/BACKEND/src/main/java/com/example/demo/security/config/SecurityConfiguration.java
+++ CONTROL/BACKEND/src/main/java/com/example/demo/security/config/SecurityConfiguration.java
@@ -19,56 +19,28 @@ public class SecurityConfiguration {
     private final JwtAuthenticationFilter jwtAuthFilter;
 
     private final AuthenticationProvider authenticationProvider;
-
+//     public SecurityConfiguration(JwtAuthenticationFilter jwtAuthFilter, AuthenticationProvider authenticationProvider) {
+//         this.jwtAuthFilter = jwtAuthFilter;
+//         this.authenticationProvider = authenticationProvider;
+//     }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
         http
-                .csrf(AbstractHttpConfigurer::disable);
-        http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         authorizeRequests ->{
                             authorizeRequests
-
-
-
-                                    .requestMatchers("api/v1/admin/authenticate").permitAll()
-                                    .requestMatchers("/api/v1/super_admin/authenticate").permitAll()
-
-                                    .requestMatchers("api/v1/home/show_forms").permitAll()
-                                    .requestMatchers("api/v1/home/show_laboratories").permitAll()
-                                    .requestMatchers("api/v1/home/show_news").permitAll()
-                                    .requestMatchers("api/v1/home/show_science_committees").permitAll()
-                                    .requestMatchers("api/v1/admin//fileSystem/{fileName}").permitAll()
-                                    
-                                    
-                                    .requestMatchers("/api/v1/super_admin/change_password").hasAuthority("SUPER")
-                                    .requestMatchers("/api/v1/super_admin/add_admin").hasAuthority("SUPER")
-                                    .requestMatchers("/api/v1/super_admin/delete_admin").hasAuthority("SUPER")
-                                    .requestMatchers("/api/v1/super_admin/show_admins").hasAuthority("SUPER")
-                                    .requestMatchers("/api/v1/super_admin/update_admin").hasAuthority("SUPER")
-
-
-
-                                    
-                                    .requestMatchers("api/v1/admin/change_password").hasAuthority("ADMIN")
-                                    .requestMatchers("api/v1/admin/add_form").hasAuthority("ADMIN")
-                                    .requestMatchers("api/v1/admin/add_laboratory").hasAuthority("ADMIN")
-                                    .requestMatchers("api/v1/admin/add_news").hasAuthority("ADMIN")
-                                    .requestMatchers("api/v1/admin/add_science_committee").hasAuthority("ADMIN")
-                                    .requestMatchers("api/v1/admin/update_form").hasAuthority("ADMIN")
-                                    .requestMatchers("api/v1/admin/update_laboratory").hasAuthority("ADMIN")
-                                    .requestMatchers("api/v1/admin/update_news").hasAuthority("ADMIN")
-                                    .requestMatchers("api/v1/admin/update_science_committee").hasAuthority("ADMIN")
-                                    .requestMatchers("api/v1/admin/show_forms").hasAuthority("ADMIN")
-                                    .requestMatchers("api/v1/admin/show_laboratories").hasAuthority("ADMIN")
-                                    .requestMatchers("api/v1/admin/show_news").hasAuthority("ADMIN")
-                                    .requestMatchers("api/v1/admin/show_science_committees").hasAuthority("ADMIN")
-                                    .requestMatchers("api/v1/admin/delete_form").hasAuthority("ADMIN")
-                                    .requestMatchers("api/v1/admin/delete_laboratory").hasAuthority("ADMIN")
-                                    .requestMatchers("api/v1/admin/delete_news").hasAuthority("ADMIN")
-                                    .requestMatchers("api/v1/admin/delete_science_committee").hasAuthority("ADMIN");
-
-
+                            
+                            
+                                .requestMatchers("/api/v1/home/show_forms", 
+                                "/api/v1/home/show_laboratories", 
+                                "/api/v1/home/show_news", 
+                                "/api/v1/home/show_science_committees").permitAll()
+                                
+                                .requestMatchers("/api/v1/**").authenticated()
+                                
+  
+                                .requestMatchers("/**").permitAll();
                             http.authenticationProvider(authenticationProvider)
                                     .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
